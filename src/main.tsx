d26c266bd150f9810da510d5032c568e06b42c96
Bas <basarab.o@icloud.com>
2025-12-04 14:14:58 +0200

    add searchbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/App/App.tsx";
-createRoot(document.getElementById("root")!).render(<App />);
+import { StrictMode } from "react";
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
